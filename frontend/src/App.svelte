887ee6819523a697233cd98de85962a7ec1bfafd
JoshuaTimothyW <joshuatimothywongso@gmail.com>
2021-08-16 19:18:18 +0700

    init frontend

---
+++ frontend/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Router,{replace} from "svelte-spa-router";
+
+	import Main from "./views/Main.svelte";
+	import Edit from "./views/Edit.svelte";
+
+	const routes = {
+		'/':Edit,
+		'/edit':Edit,
+	}
+
+</script>
+
+<main class="container-fluid">
+	<Router {routes}></Router>
+</main>
